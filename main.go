3de02e88a02fa03d0d8cb52ebf8525518d283d32
Zoltan Szabo <szabozoltan6x@gmail.com>
2025-09-14 05:16:31 +0200

    refactor: use functions to get repos, commits, branches, commit details

--- main.go
+++ main.go
@@ -3,51 +3,39 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/google/go-github/v74/github"
 )
 
-func Last24HourChangesOnBranch(rs github.RepositoriesService, branch string, ctx context.Context, owner string, repo string) {
-
+func GetReposByUser(rs github.RepositoriesService, ctx context.Context, user string) []*github.Repository {
+	opt := &github.RepositoryListByUserOptions{Type: "owner", Sort: "updated", Direction: "desc"}
+	log.Println("Fetching repos for user:", user)
+	repos, _, err := rs.ListByUser(ctx, user, opt)
+	log.Println("Number of repos found:", len(repos))
+	if err != nil {
+		log.Fatalln(err)
+		return nil
+	}
+	if len(repos) == 0 {
+		log.Println("No repos found for user:", user)
+		return nil
+	}
+	return repos
 }
 
-func main() {
-	client := github.NewClient(nil).WithAuthToken("")
-	rs := client.Repositories
-	ctx := context.Background()
-	user := "fijizxli"
-	opt := &github.RepositoryListByUserOptions{Type: "owner", Sort: "updated", Direction: "desc"}
-	RepoService := github.RepositoriesService(*rs)
-
-	//GET REPOS
-	repos, _, err := RepoService.ListByUser(ctx, user, opt)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	//LIST REPOS
-	for _, repo := range repos {
-		fmt.Printf("Repo: %s, ID: %d\n", repo.GetName(), repo.GetID())
-	}
-
-	testrepo := repos[0]
-	fmt.Printf("testrepo: %s", testrepo.GetName())
-
-	//GET BRANCHES
+func GetBranchesOnRepo(rs github.RepositoriesService, ctx context.Context, owner string, repo string) []*github.Branch {
 	var branches []*github.Branch
 	branchOpts := &github.BranchListOptions{
-		ListOptions: github.ListOptions{PerPage: 2},
+		ListOptions: github.ListOptions{PerPage: 3},
 	}
 	branchOpts.Page = 1
 	for {
-		b, resp, err := rs.ListBranches(ctx, user, testrepo.GetName(), branchOpts)
-		for i := range b {
-			fmt.Printf("Branch: %s\n", b[i].GetName())
-		}
+		log.Println("Fetching branches for repo:", repo, "page:", branchOpts.Page)
+		b, resp, err := rs.ListBranches(ctx, owner, repo, branchOpts)
 		if err != nil {
-			fmt.Errorf("fetch branches: %w", err)
+			log.Fatalln("fetch branches: %w", err)
 		}
 		branches = append(branches, b...)
 		if resp.NextPage == 0 {
@@ -55,48 +43,126 @@ func main() {
 		}
 		branchOpts.Page = resp.NextPage
 	}
+	log.Println("Number of branches found:", len(branches))
+	return branches
+}
 
-	testbranch := branches[0]
-	fmt.Printf("testbranch: %s", testbranch.GetName())
-
-	//GET COMMITS
+func GetCommitsOnBranch(rs github.RepositoriesService, branch string, ctx context.Context, owner string, repo string, since time.Time) []*github.RepositoryCommit {
 	commitPerPageListOptions := &github.ListOptions{PerPage: 10}
 	commitPerPageListOptions.Page = 1
+	allCommits := []*github.RepositoryCommit{}
 	for {
-		since := time.Now().Add(-24 * time.Hour)
-		commitOpt := &github.CommitsListOptions{Author: user, ListOptions: *commitPerPageListOptions, SHA: testbranch.GetName(), Since: since}
-		commits, resp, err := client.Repositories.ListCommits(ctx, user, testrepo.GetName(), commitOpt)
-		fmt.Println("Commits:", len(commits))
-		fmt.Println("resp:", resp.Status)
+		commitOpt := &github.CommitsListOptions{Author: owner, ListOptions: *commitPerPageListOptions, SHA: branch, Since: since}
+		log.Println("Fetching commits for branch:", branch, "page:", commitPerPageListOptions.Page)
+		commits, resp, err := rs.ListCommits(ctx, owner, repo, commitOpt)
+		allCommits = append(allCommits, commits...)
+		log.Printf("Fetched %d commits, response: %s\n", len(commits), resp.Status)
 
 		if len(commits) == 0 {
-			fmt.Println("No commits in the last 24 hours")
+			log.Println("No commits in the last 24 hours")
+			return nil
+		}
+		if err != nil {
+			log.Fatalln(err)
+			return nil
+		}
+
+		if resp.NextPage == 0 {
 			break
 		}
 
+		commitPerPageListOptions.Page = resp.NextPage
+	}
+	log.Println("Number of commits found:", len(allCommits))
+	return allCommits
+}
+
+func GetCommitDetails(rs github.RepositoriesService, ctx context.Context, owner string, repo string, sha string, commits []*github.RepositoryCommit) []*github.RepositoryCommit {
+	allCommits := []*github.RepositoryCommit{}
+	for _, commit := range commits {
+		log.Printf("Commit: %s, Author: %s, Date: %s\n", commit.GetSHA(), commit.GetAuthor().GetLogin(), commit.GetCommit().GetAuthor().GetDate())
+		log.Printf("Message: %s\n", commit.GetCommit().GetMessage())
+		commitDetails, _, err := rs.GetCommit(ctx, owner, repo, *commit.SHA, nil)
 		if err != nil {
 			fmt.Println(err)
-			return
+			return nil
 		}
+		for _, file := range commitDetails.Files {
+			log.Printf("Files changed: %d\n", commitDetails.GetStats().GetTotal())
+			log.Printf("- %s\n", file.GetFilename())
+			log.Printf("  Additions: %d, Deletions: %d, Changes: %d\n", file.GetAdditions(), file.GetDeletions(), file.GetChanges())
+			log.Println("Filename:", file.GetFilename())
+			log.Println("diff:")
+			log.Println(file.GetPatch())
+		}
+		allCommits = append(allCommits, commitDetails)
+	}
+	return allCommits
+}
 
-		for _, commit := range commits {
-			fmt.Printf("Commit: %s, Author: %s, Date: %s\n", commit.GetSHA(), commit.GetAuthor().GetLogin(), commit.GetCommit().GetAuthor().GetDate())
-			fmt.Printf("Message: %s\n", commit.GetCommit().GetMessage())
-			fmt.Println("File names:")
-			commitDetails, _, err := rs.GetCommit(ctx, user, testrepo.GetName(), *commit.SHA, nil)
+func main() {
+	client := github.NewClient(nil).WithAuthToken("")
+	rs := client.Repositories
+	ctx := context.Background()
+	user := "fijizxli"
+	RepoService := github.RepositoriesService(*rs)
+	testrepo := "changelogic"
+	testbranch := "main"
+
+	/* using static testrepo and testbranch for easier testing and to reduce API calls
+	repos := GetReposByUser(RepoService, ctx, user)
+	testrepo := repos[0].GetName()
+	branches := GetBranchesOnRepo(RepoService, ctx, user, testrepo)
+	testbranch := branches[0].GetName()
+	*/
+	since := time.Now().Add(-24 * time.Hour)
+	commits := GetCommitsOnBranch(RepoService, testbranch, ctx, user, testrepo, since)
+	log.Println("Commits in the last 24 hours:", len(commits))
+
+	GetCommitDetails(RepoService, ctx, user, testrepo, testbranch, commits)
+
+	//GET COMMITS
+	/*
+		commitPerPageListOptions := &github.ListOptions{PerPage: 10}
+		commitPerPageListOptions.Page = 1
+		for {
+			since := time.Now().Add(-24 * time.Hour)
+			commitOpt := &github.CommitsListOptions{Author: user, ListOptions: *commitPerPageListOptions, SHA: testbranch, Since: since}
+			commits, resp, err := client.Repositories.ListCommits(ctx, user, testrepo, commitOpt)
+			//fmt.Println("Commits:", len(commits))
+			//fmt.Println("resp:", resp.Status)
+
+			if len(commits) == 0 {
+				fmt.Println("No commits in the last 24 hours")
+				break
+			}
+
 			if err != nil {
 				fmt.Println(err)
 				return
 			}
-			for _, file := range commitDetails.Files {
-				fmt.Printf("Files changed: %d\n", commitDetails.GetStats().GetTotal())
-				fmt.Printf("- %s\n", file.GetFilename())
-				fmt.Printf("  Additions: %d, Deletions: %d, Changes: %d\n", file.GetAdditions(), file.GetDeletions(), file.GetChanges())
+
+			for _, commit := range commits {
+				fmt.Printf("Commit: %s, Author: %s, Date: %s\n", commit.GetSHA(), commit.GetAuthor().GetLogin(), commit.GetCommit().GetAuthor().GetDate())
+				fmt.Printf("Message: %s\n", commit.GetCommit().GetMessage())
+				commitDetails, _, err := rs.GetCommit(ctx, user, testrepo, *commit.SHA, nil)
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				for _, file := range commitDetails.Files {
+					fmt.Printf("Files changed: %d\n", commitDetails.GetStats().GetTotal())
+					fmt.Printf("- %s\n", file.GetFilename())
+					fmt.Printf("  Additions: %d, Deletions: %d, Changes: %d\n", file.GetAdditions(), file.GetDeletions(), file.GetChanges())
+					fmt.Println("Filename:", file.GetFilename())
+					fmt.Println("diff:")
+					fmt.Println(file.GetPatch())
+				}
 			}
+			if resp.NextPage == 0 {
+				break
+			}
+			commitPerPageListOptions.Page = resp.NextPage
 		}
-		if resp.NextPage == 0 {
-			break
-		}
-		commitPerPageListOptions.Page = resp.NextPage
-	}
+	*/
 }
